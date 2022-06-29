d048770a3095541e3d6c2658574948944cda3e39
Rahman Ertürk <rahman.erturk@Rahman-Air.fritz.box>
2022-06-29 14:41:05 +0200

    Aufgaben gemacht

---
+++ mathematik/index.js
@@ -0,0 +1,58 @@
+// Aufgabe A
+
+const wasserTiefe = 300;
+const wasserMenge = 3750;
+const keinWettkmäpfeWasserTiefe = wasserTiefe - 5;
+const keinWettkämpfeWasserMenge = keinWettkmäpfeWasserTiefe * wasserMenge / wasserTiefe;
+
+console.log(keinWettkämpfeWasserMenge)
+
+
+// Aufgabe B
+
+const einLiterDistance = 100 / 8;
+const tankfüllung = 66;
+const tankfüllungDistance = einLiterDistance * tankfüllung;
+
+console.log(tankfüllungDistance)
+
+
+
+// Aufgabe C
+
+const anzahlArbeiter = 3;
+const dreiArbeiterDauer = 17;
+const anzahlArbeiterZwei = 5;
+const fünfArbeiterDauer =  dreiArbeiterDauer * anzahlArbeiter / anzahlArbeiterZwei;
+
+console.log(fünfArbeiterDauer)
+
+
+
+// Aufgabe D
+
+const gleichD = 882 / 14 * 12 / 2.3 * 999;
+
+console.log(gleichD)
+
+
+
+// Aufgabe E
+
+const gleichE = (12 * 7 * 17 / 7 / 17) + 333;
+
+console.log(gleichE)
+
+
+
+// Aufgabe F
+
+const gartenzaunLange = 1680;
+const anzahlSockel = gartenzaunLange / 120;
+const kostenSockel = 24.40
+const totalGeld = anzahlSockel * kostenSockel;
+
+console.log(totalGeld)
+
+
+
---
+++ variablen-datentype/index.js
@@ -0,0 +1,10 @@
+const vorname = "Rahman";
+const land = "der Türkei";
+const essen = "Manti";
+const essenZwei = "Sarma";
+const alter = 25;
+const geburtsJahr = 1996;
+
+console.log("Hallo ich bin" + " " + vorname + " " + "und ich komme aus" + " " + land + ".")
+console.log("Meine Lieblingsspeisen sind" + " " + essen + " " + "und" + " " + essenZwei + ".")
+console.log("Ich bin " + alter + " Jahre alt und " + geburtsJahr + " geboren.")
