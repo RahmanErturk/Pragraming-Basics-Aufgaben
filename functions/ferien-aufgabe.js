aae7f46a6a766a6706413224158162fcd7720fc7
Rahman Ertürk <rahman.erturk@gmail.com>
2022-07-20 16:26:20 +0200

    erste Aufgabe gemacht

--- functions/ferien-aufgabe.js
+++ functions/ferien-aufgabe.js
@@ -73,19 +73,71 @@ function incrementString (strng) {
 
 const cards = ["2 Pik","3 Pik","4 Pik","5 Pik","6 Pik", "7 Pik", "8 Pik", "9 Pik", "10 Pik", "Bube Pik", "Dame Pik", "König Pik", "Ass Pik", "2 Kreuz","3 Kreuz","4 Kreuz","5 PKreuzik","6 Kreuz", "7 Kreuz", "8 Kreuz", "9 Kreuz", "10 Kreuz", "Bube Kreuz", "Dame Kreuz", "König Kreuz", "Ass Kreuz", "2 Karo","3 Karo","4 Karo","5 Karo","6 Karo", "7 Karo", "8 Karo", "9 Karo", "10 Karo", "Bube Karo", "Dame Karo", "König Karo", "Ass Karo", "2 Herz","3 Herz","4 Herz","5 Herz","6 Herz", "7 Herz", "8 Herz", "9 Herz", "10 Herz", "Bube Herz", "Dame Herz", "König Herz", "Ass Herz"]
 
-function getCard(myCard){
+const shuffledCards = [];
 
-    let cardsOfArray = []
-    for (let i = 0; i < myCard.length; i++){
+function shuffleCards(myCard) {
+    for (let i = myCard.length + 1; i > 0; i--) {
+        const indexNum = parseInt(Math.random()*(myCard.length));
+        // console.log(indexNo);
+        shuffledCards.push(myCard.splice(indexNum, 1).join(""));
         
-        const index = parseInt(Math.random()*(myCard.length))
-        
-        const newCard = myCard.splice(index, 1)
-        
-        cardsOfArray.push(newCard)
-    }
-    return cardsOfArray.join(", ")
-} 
+    };
+    return shuffledCards;
+}
 
-console.log(getCard(cards))
-console.log(cards)
+console.log(shuffleCards(cards));
+
+function getCard(myCard) {
+    
+    for (let i = myCard.length + 1; i > 0; i--) {
+        if(myCard.length > 0){
+            const indexNo = parseInt(Math.random()*(myCard.length));
+            // console.log(indexNo);
+            let newCard = myCard.splice(indexNo, 1).join("");
+            console.log(newCard);
+        } else {
+            console.log({shuffledCards});
+            return "Der Stapel ist leer";
+        };
+    };
+};
+console.log(getCard(shuffledCards));
+
+// Bonus:
+// Implementiere eine Funktion mit dem Namen shuffleCards welche die Karten im Array cards mischt.
+
+// Aufgabe 2 (Passwort Generator)
+// Implementiere eine Funktion mit dem Namen createPassword welche ein zufällig erzeugtes Passwort zurückgibt.
+// Die Funktion gibt einen string zurück
+// Die Funktion erhält 4 Parameter
+// 1. length, eine number, gibt die Länge des Passworts an
+// 2. useSmallLetters, ein boolean, gibt an ob das Passwort kleine Buchstaben von a-z enthalten soll
+// 3. useCapitalLetters, ein boolean, gibt an ob das Passwort große Buchstaben von A-Z enthalten soll
+// 4. useSpecialCharacters, ein boolean, gibt an ob das Passwort Sonderzeichen wie @ ! $ % & * enthalten soll.
+
+
+
+
+
+
+
+
+
+
+//Aufgabe 3 (Symmetrischer Unterschied)
+
+// Implementiere eine Funktion mit dem namen symmetricDifference(), die zwei Arrays als Parameter erhält und ein Array mit deren symmetrischer Differenz zurückgibt. Das zurückgegebene Array darf nur eindeutige Werte enthalten (keine Duplikate).
+// Beispiel:
+// symmetricDifference([1, 2], [ ]) sollte [1, 2] zurückgeben
+// symmetricDifference([1, 2], [1]) sollte [2] zurückgeben
+// symmetricDifference([1, 2], [1, 3]) sollte [2, 3] zurückgeben
+// symmetricDifference([1, 2, 3], [5, 2, 1, 4]) sollte [3, 4, 5] zurückgeben
+// symmetricDifference([1, 2, 3, 3], [5, 2, 1, 4]) sollte [3, 4, 5] zurückgeben
+// symmetricDifference([1, 2, 3], [5, 2, 1, 4, 5]) sollte [3, 4, 5] zurückgeben
+
+// function diffy(a, b) {
+//     return [
+//       ...a.filter((x) => !b.includes(x)),
+//       ...b.filter((x) => !a.includes(x)),
+//     ];
+// }
